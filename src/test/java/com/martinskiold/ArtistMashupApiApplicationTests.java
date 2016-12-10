c9ebd5c6726aaabf94377cde2b8a07adbb339c3e
Martin Skiöld <martinskiold@Martins-MacBook-Air.local>
2016-12-10 18:09:52 +0100

    Refactored

--- src/test/java/com/martinskiold/ArtistMashupApiApplicationTests.java
+++ src/test/java/com/martinskiold/ArtistMashupApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CygniBackendMartinskioldApplicationTests {
+public class ArtistMashupApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
